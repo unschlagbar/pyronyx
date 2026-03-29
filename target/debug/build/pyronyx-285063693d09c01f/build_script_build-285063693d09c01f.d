0ce29600f28b59041d7e4416d690b17d6253e157
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2026-03-29 20:50:53 +0200

    Delete target directory

--- target/debug/build/pyronyx-285063693d09c01f/build_script_build-285063693d09c01f.d
+++
@@ -1,7 +0,0 @@
-/home/unschlagbar/Dev/gg/Pyronxy/target/debug/build/pyronyx-285063693d09c01f/build_script_build-285063693d09c01f.d: build.rs
-
-/home/unschlagbar/Dev/gg/Pyronxy/target/debug/build/pyronyx-285063693d09c01f/build_script_build-285063693d09c01f: build.rs
-
-build.rs:
-
-# env-dep:ANDROID_NDK_HOME
